427804ff7ea0cc1e7aa8e7b2b6b83189ea5c356e
Chris Lane <chris.lane@thinkmill.com.au>
2024-12-13 11:42:11 +1100

    Update route example

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,5 @@
-import { Prose } from '@ag.ds-next/react/prose';
+import { HomePageContent } from '../components/HomePageContent';
 
-export default function NotFoundPage() {
-	return (
-		<Prose>
-			<h1>AG Design System Starter Kit</h1>
-			<p>
-				This is a simple starter kit built using the AG Design System, NextJS
-				and Typescript.
-			</p>
-			<p>
-				For examples of common user interfaces, please refer to the{' '}
-				<a href="https://design-system.agriculture.gov.au/templates">
-					design system templates.
-				</a>
-				.
-			</p>
-		</Prose>
-	);
+export default function HomePage() {
+	return <HomePageContent />;
 }
---
+++ components/HomePageContent/HomePageContent.stories.tsx
@@ -0,0 +1,12 @@
+import { HomePageContent } from './HomePageContent';
+
+const meta = {
+	title: 'Pages/Home',
+	component: HomePageContent,
+};
+
+export default meta;
+
+export const Default = () => {
+	return <HomePageContent />;
+};
